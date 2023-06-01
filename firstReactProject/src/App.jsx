3da1ceb7046a9002d9560b3f1997a3d8498afa6a
Oleksandr Oboznyi <sasha.obozny@gmail.com>
2023-06-01 12:37:13 +0300

    refactored App.jsx

--- firstReactProject/src/App.jsx
+++ firstReactProject/src/App.jsx
@@ -1,19 +1,11 @@
 import { Title } from "./components/Title/Title";
-import { useDispatch } from "react-redux";
-import { increment } from "./redux/counterSlice";
 import Increment from "./components/Increment/Increment";
 import Decrement from "./components/Decrement/Decrement";
 import Reset from "./components/Reset/Reset";
 import Counter from "./components/Counter/Counter";
 import './main.scss'
 
-
-
 function App() {
-  // const dispatch = useDispatch();
-  // const handleIncrement = () => {
-  //   dispatch(increment)
-  // }
   return (      
       <div className="center">
         <Title className = 'title'>СounterSlice з використанням Redux Toolkit</Title>
